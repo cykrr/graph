b1984e9bc2270d9c992f551b997d618116e0a4d1
benja robles <benjaa.robless@gmail.com>
2022-01-16 12:14:10 -0300

    global cam

--- include/camera.hpp
+++ include/camera.hpp
@@ -1,18 +1,26 @@
 #ifndef CAMERA_H
 #	define CAMERA_H
 #include <glm/vec3.hpp>
+#include <glm/glm.hpp>
+#include <iostream>
 class Camera { 
-	bool first_mouse_cam;
+
+
+	float 
+		yaw, pitch,
+		last_x, last_y;
+
+	public:
+		Camera();
+		void set_speed(float speed);
+		float speed, FOV;
 
 	glm::vec3 
 		position, front, up;
 
-	float 
-		yaw, pitch,
-		last_x, last_y,
-		FOV;
 	public:
-		Camera();
+		bool first_mouse_cam;
+		void update(double, double);
 
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,110 @@ const unsigned int SCR_HEIGHT = 600;
 
 #include "camera.hpp"
 #include "window_manager.hpp"
+#include "shaders.hpp"
+#include "program.hpp"
+
 
 int main () {
-	Camera* cam = new Camera();
 	WindowManager* wm = new WindowManager();
+	Program* program = new Program();
+
+	    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+
+
+	unsigned int VAO, VBO;
+	/* gen vao and vbo */
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	program->use();
+
+
+
+	while (!glfwWindowShouldClose(wm->window)){
+		wm->update_dt();
+		wm->process_input();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+
+		program->use();
+		
+		 glm::mat4 projection = glm::perspective(glm::radians(wm->cam->FOV), 
+				 wm->get_width() / wm->get_height(), 0.1f, 100.0f);
+		
+		 program->set_mat4("projection", projection);
+
+		 glm::mat4 view = glm::lookAt(wm->cam->position, wm->cam->position + wm->cam->front, 
+				 wm->cam->up);
+		 program->set_mat4("view", view);
+
+
+		 glBindVertexArray(VAO);
+
+		 glm::mat4 model = glm::mat4(1.0f);
+		 program->set_mat4("model", model);
+ 		 glDrawArrays(GL_TRIANGLES, 0, 36);
+		 model = glm::translate(model, glm::vec3(1.0f, 0.0f, 0.0f));
+		 program->set_mat4("model", model);
+ 		 glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+
+		glfwSwapBuffers(wm->window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
 	return 0;
 }
