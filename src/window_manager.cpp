660521aa534d8b74d5f3c352a88f8b0b322ff5fc
benja robles <benjaa.robless@gmail.com>
2022-01-16 12:13:37 -0300

    init glad

--- src/window_manager.cpp
+++ src/window_manager.cpp
@@ -3,13 +3,17 @@ WindowManager::WindowManager(){
 	this->delta_time = 0.0f;
 	this->last_frame = 0.0f;
 
-	
+	this->cam = new Camera();
 	init_gl();
 
 }
 
 void WindowManager::framebuffer_callback(GLFWwindow* window, int x, int y){
 	glViewport(0, 0, x, y);
+}
+
+void WindowManager::mouse_callback(GLFWwindow* window, double pos_x, double pos_y){
+	cam->update(pos_x, pos_y);
 }
 
 void WindowManager::init_gl(){
@@ -34,6 +38,14 @@ void WindowManager::init_gl(){
 
 	glfwMakeContextCurrent(this->window);
 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        exit(1);
+    }
+
+
+
 	glfwSetWindowUserPointer(this->window, this);
 
 	auto func = [](GLFWwindow* w, int x, int y){
@@ -42,5 +54,47 @@ void WindowManager::init_gl(){
 
 	glfwSetFramebufferSizeCallback(this->window, func);
 
+	auto mouse_func = [](GLFWwindow* w, double pos_x, double pos_y){
+		static_cast<WindowManager*>(glfwGetWindowUserPointer(w))->mouse_callback(w, pos_x, pos_y);
+	};
+
+	glfwSetCursorPosCallback(this->window, mouse_func);
+	glfwSetInputMode(this->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glEnable(GL_DEPTH_TEST);
+
+}
+
+void WindowManager::update_dt(){
+	float current_frame = glfwGetTime();
+	this->delta_time = current_frame - this->last_frame;
+	this->last_frame = current_frame;
+}
+
+void WindowManager::process_input()
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    this->cam->speed = 2.5f * this->delta_time;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        this->cam->position += cam->speed * cam->front;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        this->cam->position -= cam->speed * cam->front;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        this->cam->position -= glm::normalize(glm::cross(cam->front, cam->up)) * cam->speed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        this->cam->position += glm::normalize(glm::cross(cam->front, cam->up)) * cam->speed;
+}
+
+float WindowManager::get_width(){
+	int width, height;
+	glfwGetWindowSize(this->window, &width, &height);
+	return width;
+}
+
+float WindowManager::get_height(){
+	int width, height;
+	glfwGetWindowSize( this->window, &width, &height);
+	return height;
 
 }
