71159380de3aa0889c4cb9fffe5a974d68865142
benja robles <benjaa.robless@gmail.com>
2022-01-16 12:14:02 -0300

    wm decl

--- include/window_manager.hpp
+++ include/window_manager.hpp
@@ -1,7 +1,11 @@
 #ifndef WM_H
 #define WM_H
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "camera.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/glm.hpp"
 class WindowManager{
 		float delta_time, last_frame;
 	public:
@@ -10,7 +14,14 @@ class WindowManager{
 		void init_gl();
 
 		void framebuffer_callback(GLFWwindow* window, int x, int y );
+		void mouse_callback(GLFWwindow* window, double pos_x, double pos_y);
+
+		void process_input();
+
+		float get_width(), get_height();
 		
 		GLFWwindow*  window;
+		Camera* cam;
+	private:
 };
 #endif
