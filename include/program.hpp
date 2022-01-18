f891456614ec171d94d0a730526a08d67faffc2d
benja robles <benjaa.robless@gmail.com>
2022-01-18 14:01:54 -0300

    edit program so we can have multiple instances with diff shaders

--- include/program.hpp
+++ include/program.hpp
@@ -1,14 +1,22 @@
 #ifndef PROGRAM_H
 #define PROGRAM_H
 #include "glm/mat4x4.hpp"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 #include <iostream>
 
 #include "shaders.hpp"
 class Program{
 	public:
-		Program();
+		/* receives the vertex and the fragment names to be used */
+		Program(std::string, std::string);
 		void use();
 		void set_mat4(std::string name, glm::mat4 & matrix);
+		void set_vec3(std::string name, glm::vec3 & vec);
+		void set_vec3(std::string name, glm::vec3  vec);
+
+		unsigned int VAO, VBO;
+
 	private:
 		void check_compile_errors(Shader* shader);
 
@@ -16,6 +24,8 @@ class Program{
 		Shader* fragment;
 
 		unsigned int program;
+
+		void init_vertex_arrays();
 };
 
 #endif
--- src/program.cpp
+++ src/program.cpp
@@ -1,8 +1,8 @@
 #include "program.hpp"
 
-Program::Program(){
-	this->vertex = new Shader("vertex", GL_VERTEX_SHADER);
-	this->fragment = new Shader("fragment", GL_FRAGMENT_SHADER);
+Program::Program(std::string vertex, std::string fragment){
+	this->vertex = new Shader(vertex, GL_VERTEX_SHADER);
+	this->fragment = new Shader(fragment, GL_FRAGMENT_SHADER);
 	this->program = glCreateProgram();
 
 	glAttachShader(this->program, this->vertex->shader);
@@ -10,9 +10,13 @@ Program::Program(){
 	glLinkProgram(this->program);
 	check_compile_errors(this->vertex);
 	check_compile_errors(this->fragment);
+
+	init_vertex_arrays();
 }
 void Program::use(){
 	glUseProgram(this->program);
+	glBindVertexArray(this->VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
 }
 
 void Program::set_mat4(std::string name, glm::mat4 & matrix){
@@ -30,3 +34,18 @@ void Program::check_compile_errors(Shader* shader){
 			info_log << "\n -- --------------------------------------------------- -- " << std::endl;
 	}
 }
+
+void Program::set_vec3(std::string name, glm::vec3 vec){
+	int location = glGetUniformLocation(this->program, name.c_str());
+	glUniform3fv(location, 1, &vec[0]);
+
+}
+
+void Program::init_vertex_arrays(){
+	glGenVertexArrays(1, &(this->VAO) );
+	glGenBuffers(1, &(this->VBO));
+
+	glBindVertexArray(this->VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+}
+
