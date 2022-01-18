341ec167feed1ff71734c9843f041baedce95c2b
benja robles <benjaa.robless@gmail.com>
2022-01-18 14:01:22 -0300

    add new element (cube)

--- src/main.cpp
+++ src/main.cpp
@@ -5,102 +5,114 @@ const unsigned int SCR_HEIGHT = 600;
 #include "window_manager.hpp"
 #include "shaders.hpp"
 #include "program.hpp"
+#include "color.hpp"
+#include "element.hpp"
+
+#include "plane.hpp"
 
 
 int main () {
 	WindowManager* wm = new WindowManager();
-	Program* program = new Program();
+	Program* program = new Program("plane.vs", "plane.fs");
+	
+//	Plane* plane = new Plane();
 
+	Element* element = new Element();
+
+	float cube_vertices[] = {
+		-0.5f, -0.5f, -0.5f,  
+		 0.5f, -0.5f, -0.5f,  
+		 0.5f,  0.5f, -0.5f,  
+		 0.5f,  0.5f, -0.5f,  
+		-0.5f,  0.5f, -0.5f,  
+		-0.5f, -0.5f, -0.5f,  
+		-0.5f, -0.5f,  0.5f,  
+		 0.5f, -0.5f,  0.5f,  
+		 0.5f,  0.5f,  0.5f,  
+		 0.5f,  0.5f,  0.5f,  
+		-0.5f,  0.5f,  0.5f,  
+		-0.5f, -0.5f,  0.5f,  
+		-0.5f,  0.5f,  0.5f,  
+		-0.5f,  0.5f, -0.5f,  
+		-0.5f, -0.5f, -0.5f,  
+		-0.5f, -0.5f, -0.5f,  
+		-0.5f, -0.5f,  0.5f,  
+		-0.5f,  0.5f,  0.5f,  
+
+		 0.5f,  0.5f,  0.5f,  
+		 0.5f,  0.5f, -0.5f,  
+		 0.5f, -0.5f, -0.5f,  
+		 0.5f, -0.5f, -0.5f,  
+		 0.5f, -0.5f,  0.5f,  
+		 0.5f,  0.5f,  0.5f,  
+
+		-0.5f, -0.5f, -0.5f,  
+		 0.5f, -0.5f, -0.5f,  
+		 0.5f, -0.5f,  0.5f,  
+		 0.5f, -0.5f,  0.5f,  
+		-0.5f, -0.5f,  0.5f,  
+		-0.5f, -0.5f, -0.5f,  
+
+		-0.5f,  0.5f, -0.5f,  
+		 0.5f,  0.5f, -0.5f,  
+		 0.5f,  0.5f,  0.5f,  
+		 0.5f,  0.5f,  0.5f,  
+		-0.5f,  0.5f,  0.5f,  
+		-0.5f,  0.5f, -0.5f,  
+	};
+	element->set_vertices(cube_vertices, 36, 3);
+	
 	    float vertices[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+			1.0f, 	1.0f, 0.0f, 
+			-1.0f, -1.0f, 0.0f,
+			-1.0f, 1.0f, 0.0f,
 
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-    };
+			-1.0f, -1.0f, 0.0f,
+			 1.0f,  1.0f, 0.0f,
+			 1.0f, -1.0f, 0.0f,
 
 
+	};
 
-	unsigned int VAO, VBO;
-	/* gen vao and vbo */
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	glBindVertexArray(program->VAO);
 
-	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, program->VBO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		
+	glBufferData(GL_ARRAY_BUFFER, 30*sizeof(float) , nullptr, GL_DYNAMIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof (float), (void*)0);
 	glEnableVertexAttribArray(0);
-
-	program->use();
-
 
 
 	while (!glfwWindowShouldClose(wm->window)){
 		wm->update_dt();
 		wm->process_input();
+		wm->clear();
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 
-		program->use();
 		
-		 glm::mat4 projection = glm::perspective(glm::radians(wm->cam->FOV), 
+		glm::mat4 projection = glm::perspective(glm::radians(wm->cam->FOV), 
 				 wm->get_width() / wm->get_height(), 0.1f, 100.0f);
 		
-		 program->set_mat4("projection", projection);
 
 		 glm::mat4 view = glm::lookAt(wm->cam->position, wm->cam->position + wm->cam->front, 
 				 wm->cam->up);
-		 program->set_mat4("view", view);
-
-
-		 glBindVertexArray(VAO);
 
 		 glm::mat4 model = glm::mat4(1.0f);
-		 program->set_mat4("model", model);
- 		 glDrawArrays(GL_TRIANGLES, 0, 36);
-		 model = glm::translate(model, glm::vec3(1.0f, 0.0f, 0.0f));
-		 program->set_mat4("model", model);
- 		 glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		 glm::mat4 MVP =  projection * view *  model;
+		 
+		 element->set_mat4("MVP", MVP);
+		 program->set_mat4("MVP", MVP);
+
+
+		element->draw();
+		program->use();
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 
 
