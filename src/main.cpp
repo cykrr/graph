23868453f13fc0e997d38970bf22e03d177c3899
Your Name <you@example.com>
2022-01-24 08:58:44 -0800

    use texture and alternate plane

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@ const unsigned int SCR_HEIGHT = 600;
 const float near_plane = 0.01f;
 const float far_plane = 100.0f;
 
+#include "stb_image.h"
+
 #include "camera.hpp"
 #include "window_manager.hpp"
 #include "shaders.hpp"
@@ -61,6 +63,37 @@ int main () {
 		 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+ 
+	/* set wrapping params */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	/* set texture filter params */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	/* load img */
+	int width, height, nr_channels;
+	stbi_set_flip_vertically_on_load(true);
+
+	unsigned char* data = stbi_load("wall.jpg", &width, &height, &nr_channels, 0);
+	if(data){
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	} else {
+		std::cout << "Failed to load texture;\n";
+	}
+
+
+	stbi_image_free(data);
+	
+	common_program->bare_use();
+	common_program->set_int("texture1", 0);
+
+
 	while (!glfwWindowShouldClose(wm->window)){
 		wm->update_dt();
 		wm->process_input();
@@ -87,7 +120,8 @@ int main () {
 				cube_vertices);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
-		plane->draw(&Model, &View, &Projection);
+		if(glfwGetKey(wm->window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS)
+			plane->draw(&Model, &View, &Projection);
 
 		glfwSwapBuffers(wm->window);
 		glfwPollEvents();
