e0b07d17cf33cc4fc04d51a17f57eb9a3462ca1e
Your Name <you@example.com>
2022-04-16 02:58:09 -0400

    move everything out of main, only show plane

--- src/main.cpp
+++ src/main.cpp
@@ -27,102 +27,8 @@ int main () {
 	WindowManager* wm = new WindowManager();
 	Plane* plane = new Plane();
 
-	Program* common_program = new Program("common");
-
-	Hud* hud = new Hud(wm);
-
-	glm::mat4 Projection;
-
-
-	float cube_vertices[] = {
-		 -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,          
-		  0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 
-		 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		 -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-
-
-	unsigned int texture1;
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
- 
-	/* set wrapping params */
-    glTexParameteri(GL_TEXTURE_2D,
-			GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D,
-			GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	/* set texture filter params */
-	glTexParameteri(GL_TEXTURE_2D, 
-			GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D,
-			GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	/* load img */
-	int width, height, nr_channels;
-	stbi_set_flip_vertically_on_load(true);
-
-	unsigned char* data = stbi_load("wall.jpg", 
-			&width, &height, &nr_channels, 0);
-
-	if(data){
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
-				width, height, 0, GL_RGB,
-				GL_UNSIGNED_BYTE, data);
-
-		glGenerateMipmap(GL_TEXTURE_2D);
-	} else {
-		std::cout << "Failed to load texture;\n";
-	}
-
-
-	stbi_image_free(data);
-	
-	common_program->bare_use();
-	common_program->set_int("texture1", 0);
-
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, 
-			GL_FALSE, 3*sizeof(float), (void*)0);
-
-	glEnableVertexAttribArray(0);
-
-
 	while (!glfwWindowShouldClose(wm->window)){
 		wm->update_dt();
-		show_fps(wm->window);
 		wm->process_input();
 		wm->clear();
 
@@ -139,50 +45,14 @@ int main () {
 
 				wm->cam->up);
 
-		glm::mat4 Model = glm::mat4(1.0f);
-		plane->program->use();
 
-		common_program->bare_use();
-		common_program->set_mat4("View", View);
-		common_program->set_mat4("Projection", 
+                plane->program->bare_use();
+		plane->program->set_mat4("View", View);
+		plane->program->set_mat4("Projection", 
 				Projection);
 
-		glBufferSubData(GL_ARRAY_BUFFER, 0,
-				sizeof(cube_vertices), 
-				cube_vertices);
-
-		if(glfwGetKey(wm->window, GLFW_KEY_P) == 
-				GLFW_PRESS)
-			plane->draw(&Model, &View, 
-					&Projection);
-
+                plane->draw(&View, &Projection);
 	
-		
-		for(int i=0; i < 10; i++){
-			for(int j=0; j < 10; j++){
-					Model = glm::translate(
-							glm::mat4(1.0f), 
-							glm::vec3(
-								i + 0.5f,
-								0.5f,
-								j + 0.5f)
-							);
-					common_program->set_mat4("Model",
-							Model);
-
-					glDrawArrays(GL_TRIANGLES, 0, 36);
-				}
-		}
-
-		hud->update_matrix(wm->get_width(), 
-				wm->get_height()
-				);
-		hud->draw();
-
-
-
-
-
 		glfwSwapBuffers(wm->window);
 		glfwPollEvents();
 	}
