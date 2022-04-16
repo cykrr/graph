5681d0d167506721c34f9356c1637f7ff2c51107
Your Name <you@example.com>
2022-04-16 11:29:39 -0400

    add private and assign items

--- include/plane.hpp
+++ include/plane.hpp
@@ -6,17 +6,22 @@
 
 
 class Plane {
-	public: 
-		Plane();
-		static Program* program;
-		void draw();
-		unsigned int Vao, Vbo; // IDK 
-	private:
-		static size_t vertices_size;
-		static float vertices[30];
-                void createBuffers();
-                void bindBuffers();
-                void enableAttribArray();
-                void allocateVertexData();
+public: 
+
+    Plane();
+    void draw();
+
+    unsigned int Vao, Vbo; // IDK 
+    static Program* program;
+
+private:
+
+    static size_t vertices_size;
+    static float vertices[30];
+
+    void createBuffers();
+    void bindBuffers();
+    void enableAttribArray();
+    void allocateVertexData();
 };
 #endif
