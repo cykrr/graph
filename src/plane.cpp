dd7a030b08993bb8a5fb3f4f8bee5048eb3a7d4d
Your Name <you@example.com>
2022-04-16 02:57:00 -0400

    add VAs functions to plane

--- src/plane.cpp
+++ src/plane.cpp
@@ -1,28 +1,62 @@
 #include "plane.hpp"
+// init program
+// bind plane VAO
+// enable attribute pointers
 Plane::Plane(){
-	this->program = new Program("plane");
-	glBindVertexArray(this->program->VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, this->program->VBO);
-	glBufferData(GL_ARRAY_BUFFER, 200*sizeof(float), nullptr, GL_DYNAMIC_DRAW);
+        if(!this->program)
+            this->program = new Program("plane");
+        this->createBuffers();
+        this->enableAttribArray();
+        this->allocateVertexData();
+} 
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(3*sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-}
-
-void Plane::draw(glm::mat4 *Model, glm::mat4 * View, glm::mat4 *Projection){
+// use plane program
+// send matrices
+// send buffer data
+// draw
+void Plane::draw(glm::mat4 * View, glm::mat4 *Projection){
 	this->program->bare_use();
+        this->bindBuffers();
 
-	*Model = glm::mat4(1.0f);
-	this->program->set_mat4("Model", *Model);
 	this->program->set_mat4("View", *View);
 	this->program->set_mat4("Projection", *Projection);
 
-
-	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(float)*this->vertices_size, vertices);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, 
+                sizeof(float)*this->vertices_size, vertices);
 
 	glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
+void Plane::createBuffers() {
+    glGenVertexArrays(1, &Vao);
+    glGenBuffers(1, &Vbo);
+}
+
+void Plane::bindBuffers() {
+    glBindVertexArray(this->Vao);
+    glBindBuffer(GL_ARRAY_BUFFER, this->Vbo);
+}
+
+void Plane::enableAttribArray() {
+    this->bindBuffers();
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}
+
+void Plane::allocateVertexData() {
+    this->bindBuffers();
+    glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(float), this->vertices,
+            GL_STATIC_DRAW);
+}
+
+float Plane::vertices[30] = {
+		 1.0f, 	1.0f, 0.0f, 
+		-1.0f, -1.0f, 0.0f, 
+		-1.0f,  1.0f, 0.0f, 
+		-1.0f, -1.0f, 0.0f, 
+		 1.0f,  1.0f, 0.0f, 
+		 1.0f, -1.0f, 0.0f, 
+		};
+
+size_t Plane::vertices_size = 30;
+Program *Plane::program = NULL;
