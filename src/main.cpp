5fd55404dd2b423fc8a1931cb1ea6ed0125c9fd5
Benjamín Robles <benjamin.robles.a@mail.pucv.cl>
2025-07-04 01:51:33 -0400

    Remove unused headers, show fps always, update ignore

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,7 @@ const float far_plane = 100.0f;
 
 #include "camera.hpp"
 #include "window_manager.hpp"
-#include "shaders.hpp"
 #include "program.hpp"
-#include "color.hpp"
-#include "element.hpp"
 
 
 #include "hud.hpp"
@@ -22,187 +19,183 @@ const float far_plane = 100.0f;
 double lastTime=0, nbFrames=0;
 
 void show_fps(GLFWwindow *pWindow){
-    // Measure speed
-     double currentTime = glfwGetTime();
-     double delta = currentTime - lastTime;
-     nbFrames++;
-     if ( delta >= 1.0 ){ // If last cout was more than 1 sec ago
-		 std::cout << double(nbFrames) << std::endl;
+  // Measure speed
+  double currentTime = glfwGetTime();
+  double delta = currentTime - lastTime;
+  nbFrames++;
 
-         double fps = double(nbFrames) / delta;
+  double fps = 1 / delta;
 
-         std::stringstream ss;
-         ss << " [" << fps << " FPS]";
+  printf("[FPS]: %f.00\n", fps);
 
-         nbFrames = 0;
-         lastTime = currentTime;
-     }
+  nbFrames = 0;
+  lastTime = currentTime;
 }
 
 
 int main () {
-	WindowManager* wm = new WindowManager();
-	Plane* plane = new Plane();
+  WindowManager* wm = new WindowManager();
+  Plane* plane = new Plane();
 
-	Program* common_program = new Program("common");
+  Program* common_program = new Program("common");
 
-	Hud* hud = new Hud(wm);
+  Hud* hud = new Hud(wm);
 
-	glm::mat4 Projection;
-
-
-	float cube_vertices[] = {
-		 -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,          
-		  0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 
-		 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		 -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
+  glm::mat4 Projection;
 
 
-	unsigned int texture1;
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
- 
-	/* set wrapping params */
-    glTexParameteri(GL_TEXTURE_2D,
-			GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D,
-			GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	/* set texture filter params */
-	glTexParameteri(GL_TEXTURE_2D, 
-			GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D,
-			GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	/* load img */
-	int width, height, nr_channels;
-	stbi_set_flip_vertically_on_load(true);
-
-	unsigned char* data = stbi_load("wall.jpg", 
-			&width, &height, &nr_channels, 0);
-
-	if(data){
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
-				width, height, 0, GL_RGB,
-				GL_UNSIGNED_BYTE, data);
-
-		glGenerateMipmap(GL_TEXTURE_2D);
-	} else {
-		std::cout << "Failed to load texture;\n";
-	}
-
-
-	stbi_image_free(data);
-	
-	common_program->bare_use();
-	common_program->set_int("texture1", 0);
-
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, 
-			GL_FALSE, 3*sizeof(float), (void*)0);
-
-	glEnableVertexAttribArray(0);
+  float cube_vertices[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,          
+    0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+    0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+    0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+  };
 
 
-	while (!glfwWindowShouldClose(wm->window)){
-		wm->update_dt();
-		show_fps(wm->window);
-		wm->process_input();
-		wm->clear();
+  unsigned int texture1;
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
 
-		glm::mat4 Projection = glm::perspective(
-				glm::radians(wm->cam->FOV), 
-				wm->get_width()/wm->get_height(), 
-				near_plane, far_plane);
+  /* set wrapping params */
+  glTexParameteri(GL_TEXTURE_2D,
+                  GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D,
+                  GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-		glm::mat4 View = glm::lookAt(
-				wm->cam->position, 
+  /* set texture filter params */
+  glTexParameteri(GL_TEXTURE_2D, 
+                  GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D,
+                  GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-				wm->cam->position + 
-					wm->cam->front, 
+  /* load img */
+  int width, height, nr_channels;
+  stbi_set_flip_vertically_on_load(true);
 
-				wm->cam->up);
+  unsigned char* data = stbi_load("wall.jpg", 
+                                  &width, &height, &nr_channels, 0);
 
-		glm::mat4 Model = glm::mat4(1.0f);
-		plane->program->use();
+  if(data){
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+                 width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
 
-		common_program->bare_use();
-		common_program->set_mat4("View", View);
-		common_program->set_mat4("Projection", 
-				Projection);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture;\n";
+  }
 
-		glBufferSubData(GL_ARRAY_BUFFER, 0,
-				sizeof(cube_vertices), 
-				cube_vertices);
 
-		if(glfwGetKey(wm->window, GLFW_KEY_P) == 
-				GLFW_PRESS)
-			plane->draw(&Model, &View, 
-					&Projection);
+  stbi_image_free(data);
 
-	
-		
-		for(int i=0; i < 10; i++){
-			for(int j=0; j < 10; j++){
-					Model = glm::translate(
-							glm::mat4(1.0f), 
-							glm::vec3(
-								i + 0.5f,
-								0.5f,
-								j + 0.5f)
-							);
-					common_program->set_mat4("Model",
-							Model);
+  common_program->bare_use();
+  common_program->set_int("texture1", 0);
 
-					glDrawArrays(GL_TRIANGLES, 0, 36);
-				}
-		}
 
-		hud->update_matrix(wm->get_width(), 
-				wm->get_height()
-				);
-		hud->draw();
+  glVertexAttribPointer(0, 3, GL_FLOAT, 
+                        GL_FALSE, 3*sizeof(float), (void*)0);
+
+  glEnableVertexAttribArray(0);
+
+
+  while (!glfwWindowShouldClose(wm->window)){
+    wm->update_dt();
+    show_fps(wm->window);
+    wm->process_input();
+    wm->clear();
+
+    glm::mat4 Projection = glm::perspective(
+      glm::radians(wm->cam->FOV), 
+      wm->get_width()/wm->get_height(), 
+      near_plane, far_plane);
+
+    glm::mat4 View = glm::lookAt(
+      wm->cam->position, 
+
+      wm->cam->position + 
+      wm->cam->front, 
+
+      wm->cam->up);
+
+    glm::mat4 Model = glm::mat4(1.0f);
+    plane->program->use();
+
+    common_program->bare_use();
+    common_program->set_mat4("View", View);
+    common_program->set_mat4("Projection", 
+                             Projection);
+
+    glBufferSubData(GL_ARRAY_BUFFER, 0,
+                    sizeof(cube_vertices), 
+                    cube_vertices);
+
+    if(glfwGetKey(wm->window, GLFW_KEY_P) == 
+      GLFW_PRESS)
+      plane->draw(&Model, &View, 
+                  &Projection);
 
 
 
+    for(int i=0; i < 10; i++){
+      for(int j=0; j < 10; j++){
+        Model = glm::translate(
+          glm::mat4(1.0f), 
+          glm::vec3(
+            i + 0.5f,
+            0.5f,
+            j + 0.5f)
+        );
+        common_program->set_mat4("Model",
+                                 Model);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+      }
+    }
+
+    hud->update_matrix(wm->get_width(), 
+                       wm->get_height()
+                       );
+    hud->draw();
 
 
-		glfwSwapBuffers(wm->window);
-		glfwPollEvents();
-	}
-	glfwTerminate();
-	return 0;
+
+
+
+    glfwSwapBuffers(wm->window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+  return 0;
 }
